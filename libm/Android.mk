e4440a87864e1d1fc77c9c5c2e9668f271a90855
chasmodo <bosko.trkulja@gmail.com>
2014-03-22 21:08:17 +0100

    Revert "libm: use neon optimized sin/cos/pow/sqrt/sqrtf"
    This reverts commit 07e3ef587723968e4030ded447cc99a1a4468068.
    
    Change-Id: Ic12fd6a2a37b5d09b3a848cd45a6a2c11da225f2

--- libm/Android.mk
+++ libm/Android.mk
@@ -245,38 +245,7 @@ libm_common_includes := $(LOCAL_PATH)/upstream-freebsd/lib/msun/src/
 libm_arm_includes := $(LOCAL_PATH)/arm
 libm_arm_src_files := arm/fenv.c
 ifeq ($(TARGET_CPU_VARIANT),krait)
-  libm_arm_src_files += \
-	arm/e_pow.S	\
-	arm/s_cos.S	\
-	arm/s_sin.S	\
-	arm/e_sqrtf.S	\
-	arm/e_sqrt.S
-  libm_arm_cflags += -DKRAIT_NEON_OPTIMIZATION -fno-if-conversion
-else
-  ifeq ($(TARGET_USE_QCOM_BIONIC_OPTIMIZATION),true)
-    libm_arm_src_files += \
-      arm/e_pow.S \
-      arm/s_cos.S \
-      arm/s_sin.S \
-      arm/e_sqrtf.S \
-      arm/e_sqrt.S
-    libm_arm_cflags += -DKRAIT_NEON_OPTIMIZATION -fno-if-conversion
-  else
-    ifeq ($(ARCH_ARM_HAVE_NEON),true)
-      libm_arm_src_files += \
-        arm/e_pow.S \
-        arm/s_cos.S \
-        arm/s_sin.S \
-        arm/e_sqrtf.S \
-        arm/e_sqrt.S
-    else
-      libm_common_src_files += \
-        upstream-freebsd/lib/msun/src/s_cos.c \
-        upstream-freebsd/lib/msun/src/s_sin.c \
-        upstream-freebsd/lib/msun/src/e_sqrtf.c \
-        upstream-freebsd/lib/msun/src/e_sqrt.c
-    endif
-  endif
+  libm_arm_cflags += -DKRAIT_NEON_OPTIMIZATION
 endif
 
 libm_x86_includes := $(LOCAL_PATH)/i386 $(LOCAL_PATH)/i387
